c8fb67caa07e7906d13b6310520d50550d315094
Célia Chazel <chazel.celia@gmail.com>
2018-04-12 22:41:10 +0200

    Basic prototype ok

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div id="app">
-    <button @click="playAll">Jouer une note</button>
-    <h1>{{playedNote}}</h1>
-    <template v-for="note in frenchNotes">
-      <span :key='note' @click='guessResult(note)'>{{note}} </span>
-    </template>
+    <button @click="playRandom">Start</button>
+    <!-- <h1>{{playedNote}}</h1> -->
+    <div id='notes-list'>
+      <template v-for="note in frenchNotes">
+        <p class='note-choice' :key='note' @click='guessResult(note)'>{{note}} </p>
+      </template>
+    </div>
+    <h2 id='result' :style='{backgroundColor : result.color}'>{{result.text}}</h2>
   </div>
 </template>
 
@@ -27,7 +30,8 @@ export default {
         notes : ['C','D','E','F','G','A','B'],
         frenchNotes : ['do','ré','mi','fa','sol','la','si'],
         playedNote : 'Placeholder note',
-        numNote: 0
+        numNote: 0,
+        result: {text: '?', color: 'white'}
       };
     },
     methods: {
@@ -45,23 +49,36 @@ export default {
         }
       },
       playRandom: function() {
+        this.result.color = 'white'
+        this.result.text = '?'
+        let octave;
         let randomNum = Math.floor(Math.random() * 7)
+        randomNum < 5 ? octave = 4 : octave = 5
+        console.log(octave)
         let randomNote = this.notes[randomNum]
         let frenchNote = this.frenchNotes[randomNum]
         this.playedNote = frenchNote
-        synth.playNote(synth.noteToMIDI(randomNote, 4), 2.0, 1.0, 0)
+        synth.playNote(synth.noteToMIDI(randomNote, octave), 2.0, 1.0, 0)
       },
       guessResult: function(note) {
         let indexOfGuessedNote = this.frenchNotes.indexOf(note)
         let indexOfPlayedNote = this.frenchNotes.indexOf(this.playedNote)
         if (indexOfGuessedNote > indexOfPlayedNote) {
-          console.log('Note trop haute')
+          this.result.text = '<<<'
+          this.result.color = 'red'
         }
         else if (indexOfGuessedNote < indexOfPlayedNote) {
-          console.log('Note trop basse')
+          this.result.text = '>>>'
+          this.result.color = 'red'
         }
         else {
-          console.log(`Bravo, il s'agit bien d'un ${note} !`)
+          this.result.text = note
+          this.result.color = 'green'
+          let self = this
+          setTimeout(() => {
+            self.playRandom()
+          }, 2000);
+
         }
       }
     }
@@ -76,5 +93,30 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#result {
+  margin: auto;
+  width:100px;
+}
+
+#notes-list {
+  /* margin-top: 50px; */
+  width: 50%;
+  margin: auto;
+  margin-top: 50px;
+  margin-bottom: 50px;
+  display: flex;
+  flex-direction: row;
+}
+.note-choice {
+  width: 40px;
+  height: 40px;
+  border: 1px solid black;
+  margin : 5px;
+  /* align-self: auto; */
+}
+
+.note-choice:hover {
+  background-color: grey;
 }
 </style>
