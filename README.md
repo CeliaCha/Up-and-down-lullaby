8191b75989b4a7da90c78bbd84eeb7414542e53a
Célia Chazel <chazel.celia@gmail.com>
2018-04-14 12:38:32 +0200

    Add abcjs rendering

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# midi-sandbox
+# UpAndDown-Lullaby
 
 > A Vue.js project
+> Guess notes played and display them on musical partition
+Libraries : abcjs and audiosynth
 
 ## Build Setup
 
@@ -17,5 +19,3 @@ npm run build
 # build for production and view the bundle analyzer report
 npm run build --report
 ```
-
-For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
--- src/App.vue
+++ src/App.vue
@@ -2,37 +2,42 @@
   <div id="app">
     <button @click="playAll">Jouer gamme</button>
     <button @click="playRandom">Démarrer le jeu</button>
-    <h1>{{playedNote}}</h1>
+    <!-- <h1>{{playedNote.name}}</h1> -->
     <div id='notes-list'>
-      <template v-for="note in notesNames">
-        <p class='note-choice' :key='note' @click='guessResult(note)'>{{note}} </p>
+      <template v-for="note in notes">
+        <p class='note-choice' :key='note' @click='guessResult(note)'>{{note.name}} </p>
       </template>
     </div>
     <h2 id='result' :style='{backgroundColor : result.color}'>{{result.text}}</h2>
+    <div id="paper"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import abcjs from "abcjs"
+import 'abcjs/abcjs-midi.css'
 import notesList from './notesList'
 
-const AudioSynth = require('audiosynth');
-const AudioContext = window.AudioContext || window.webkitAudioContext;
-const context = new AudioContext();
-const synth = new AudioSynth(context);
-synth.setOscWave(3); 
+const AudioSynth = require('audiosynth')
+const AudioContext = window.AudioContext || window.webkitAudioContext
+const context = new AudioContext()
+const synth = new AudioSynth(context)
+synth.setOscWave(3)
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  },
+  mounted: function () {
+      let tune = "|:              "
+			abcjs.renderAbc("paper", tune, {});
+		},
   data() {
       return {
         notes : notesList,
-        playedNote : 'Placeholder note',
+        playedNote : {},
         numNote: 0,
-        result: {text: '?', color: 'white'}
+        result: { text: '?', color: 'white' },
+        tune: { notes:'|:', count : 0 }
       };
     },
     computed : {
@@ -48,8 +53,8 @@ export default {
         if (this.numNote < 8) {
           setTimeout(() => {
             let note = this.notes[this.numNote]
-            synth.playNote(synth.noteToMIDI(note.letter, note.octave), 2.0, 1.0, 0)
-            this.playedNote = note.name
+            synth.playNote(synth.noteToMIDI(note.audio, note.octave), 2.0, 1.0, 0)
+            this.playedNote = note
             self.numNote ++
             self.playAll();
           }, 1000);
@@ -60,12 +65,13 @@ export default {
         this.result.text = '?'
         let randomNum = Math.floor(Math.random() * 8)
         let note = this.notes[randomNum]
-        this.playedNote = note.name
-        synth.playNote(synth.noteToMIDI(note.letter, note.octave), 2.0, 1.0, 0)
+        this.playedNote = note
+        synth.playNote(synth.noteToMIDI(note.audio, note.octave), 2.0, 1.0, 0)
       },
       guessResult: function(note) {
-        let indexOfGuessedNote = this.notesNames.indexOf(note)
-        let indexOfPlayedNote = this.notesNames.indexOf(this.playedNote)
+        console.log(this.playedNote)
+        let indexOfGuessedNote = this.notes.indexOf(note)
+        let indexOfPlayedNote = this.notes.indexOf(this.playedNote)
         if (indexOfGuessedNote > indexOfPlayedNote) {
           this.result.text = '<<<'
           this.result.color = 'red'
@@ -75,12 +81,17 @@ export default {
           this.result.color = 'red'
         }
         else {
-          this.result.text = note
+          this.result.text = note.name
           this.result.color = 'green'
+          if (this.tune.count % 4 === 0 && this.tune.count !== 0) this.tune.notes += "|"
+          if (this.tune.count % 8 === 0 && this.tune.count !== 0) this.tune.notes += "\n"
+          this.tune.notes += note.abcjs
+          this.tune.count ++
+          abcjs.renderAbc("paper", this.tune.notes, {});
           let self = this
           setTimeout(() => {
             self.playRandom()
-          }, 2000);
+          }, 1000);
 
         }
       }
@@ -121,5 +132,9 @@ export default {
 .note-choice:hover {
   background-color: cyan;
   cursor: pointer;
+}
+#paper {
+  max-width: 600px;
+  margin:auto;
 }
 </style>
