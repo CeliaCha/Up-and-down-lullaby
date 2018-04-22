4b8be81e44eadfe18b98f8018427e2470176f2e5
Célia Chazel <chazel.celia@gmail.com>
2018-04-22 14:42:41 +0200

    Add plenty of things I had forgotten to commit sooner

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
   <div id="app">
     <b-card bg-variant="light" header="GUESS MYSTERY NOTES" class="text-center">
-      <b-button lg @click="playRandom">PLAY</b-button>
-      <p class="card-text">Score :  <span>{{score}}</span></p>
-  
-      <div id='notes-list'>
-        <b-button-group>
-          <template v-for="note in notes">
-                  <b-button :style='{backgroundColor : note.color}' :key='note.name' @click='guessResult(note)'>{{note.name}} </b-button>
+      <b-button lg @click="playRandomNote">PLAY</b-button>
+      <p>Score : <span>{{score}}</span></p>
+      <p>Erreurs : <span>{{errors}}</span></p>
+      <div id='scores-list'>
+        <template v-for="score in scoresList">
+                      <li  class='note-name' :key='score'>{{score}} </li>
           </template>
-      </b-button-group>
+      </div>
+      <div id='notes-list'>
+        <transition-group name="flip-notes">
+          <template v-for="note in notes">
+                      <span  class='note-name' :key='note.name' @click='guessNote(note)' :style='{backgroundColor : note.color}'>{{note.name}} </span>
+          </template>
+        </transition-group>
     </div>
-
-    <h4> <b-badge id='result' :style='{backgroundColor : result.color}'>{{result.text}}</b-badge></h4>
+    <h4> <b-badge v-if="result.text !=='?'" id='result' :style='{backgroundColor : result.color}'>{{result.text}}</b-badge></h4>
     <div id="paper"></div>
     </b-card> 
   </div>
@@ -22,6 +26,7 @@
   import abcjs from "abcjs"
   import 'abcjs/abcjs-midi.css'
   import notesList from './notesList'
+  import 'sonar-css/css/sonar.css'
   
   const AudioSynth = require('audiosynth')
   const AudioContext = window.AudioContext || window.webkitAudioContext
@@ -32,17 +37,19 @@
   
   export default {
     name: 'App',
-    mounted: function() {
+        mounted: function() {
       let tune = "M:4/4\n||"
       abcjs.renderAbc("paper", tune, {});
+      this.shuffleNotes()
     },
     data() {
       return {
         notes: notesList,
         playedNote: {},
-        numNote: 0,
         errors: 0,
         score: 0,
+        scoresList: [],
+        timeStart: 0,
         result: {
           text: '?',
           color: 'white'
@@ -53,6 +60,13 @@
         }
       };
     },
+    watch: {
+      scoresList: {
+        handler() {
+          console.log(this.scoresList);
+        }
+      },
+    },
     computed: {
       notesNames: function() {
         let array = []
@@ -61,19 +75,11 @@
       }
     },
     methods: {
-      playAll: function() {
-        let self = this
-        if (this.numNote < 8) {
-          setTimeout(() => {
-            let note = this.notes[this.numNote]
-            synth.playNote(synth.noteToMIDI(note.audio, note.octave), 2.0, 1.0, 0)
-            this.playedNote = note
-            self.numNote++
-              self.playAll();
-          }, 1000);
-        }
+      shuffleNotes: function() {
+        this.notes = _.shuffle(this.notes)
       },
-      playRandom: function() {
+      playRandomNote: function() {
+        this.timeStart = Date.now()
         this.result.color = 'white'
         this.result.text = '?'
         let randomNum = Math.floor(Math.random() * 8)
@@ -81,25 +87,25 @@
         this.playedNote = note
         synth.playNote(synth.noteToMIDI(note.audio, note.octave), 2.0, 1.0, 0)
       },
-      guessResult: function(note) {
-        console.log(this.playedNote)
-        let indexOfGuessedNote = this.notes.indexOf(note)
-        let indexOfPlayedNote = this.notes.indexOf(this.playedNote)
+      guessNote: function(note) {
+        synth.playNote(synth.noteToMIDI(note.audio, note.octave), 2.0, 1.0, 0)
+        let indexOfGuessedNote = notesList.indexOf(note)
+        let indexOfPlayedNote = notesList.indexOf(this.playedNote)
         if (indexOfGuessedNote > indexOfPlayedNote) {
-          this.result.text = '<<<'
+          this.result.text = 'Note plus basse'
           this.result.color = 'red'
-          this.errors ++
-          if (this.errors > 2) this.endGame()
-        } 
-        else if (indexOfGuessedNote < indexOfPlayedNote) {
-          this.result.text = '>>>'
+          this.errors++
+            if (this.errors > 2) this.endGame()
+        } else if (indexOfGuessedNote < indexOfPlayedNote) {
+          this.result.text = 'Note plus haute'
           this.result.color = 'red'
-          this.errors ++
-          if (this.errors > 2) this.endGame()
-        } 
-        else {
-          this.score += 10
-          this.result.text = note.name
+          this.errors++
+            if (this.errors > 2) this.endGame()
+        } else {
+          let timeToFind = Date.now() - this.timeStart
+          let bonusTime = (50 - Math.round(timeToFind/100))
+          bonusTime > 0 ? this.score += 10 + bonusTime : this.score += 10
+          this.result.text = 'Bravo !'
           this.result.color = 'green'
           if (this.tune.count % 4 === 0 && this.tune.count > 3) this.tune.notes += "|"
           if (this.tune.count % 12 === 0 && this.tune.count > 11) this.tune.notes += "\n"
@@ -110,13 +116,14 @@
               responsive: "resize",
             });
           let self = this
+          this.shuffleNotes()
           setTimeout(() => {
-            self.playRandom()
-          }, 1000);
   
+            self.playRandomNote()
+          }, 2500);
         }
       },
-      endGame: function () {
+      endGame: function() {
         this.tune.notes = ''
         this.tune.count = 0
         this.result.text = '?'
@@ -125,11 +132,13 @@
         abcjs.renderAbc("paper", tune, {})
         alert(`End game ! Your score : ${this.score}`)
         this.errors = 0
+        this.scoresList.push(this.score)
         this.score = 0
       }
     }
   }
 </script>
+
 
 <style>
   #app {
@@ -139,14 +148,35 @@
     text-align: center;
     color: #2c3e50;
     max-width: 600px !important;
+    margin:auto;
   }
   
   #notes-list {
+    margin:auto;
+    display: flex;
+    flex-wrap: wrap;
+    width: 210px;
     margin-top: 50px;
     margin-bottom: 50px;
   }
-  
+
+  .note-name {
+    cursor: pointer;
+    display: inline-block;
+    margin: 1px;
+    width: 50px;
+    height: 50px;
+    line-height:50px;
+    border-radius: 5px;
+  }
+
   #paper {
     max-width: 500px;
   }
+  
+  .flip-notes-move {
+    transition: transform 1s;
+  }
+  
+
 </style>
--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import BootstrapVue from 'bootstrap-vue'
-Vue.use(BootstrapVue);
-
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue);
 
+
+
+import 'sonar-css/css/sonar.min.css'
 
 
 Vue.config.productionTip = false
